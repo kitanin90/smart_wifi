4d1545b93fe084fb76e54dddcd866f7676f4412b
Viktor Plis <rigfox@gmail.com>
2019-05-05 16:35:39 +0500

    Added panel templates

--- panel/urls.py
+++ panel/urls.py
@@ -5,4 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.connect, name='connect'),
     path('successful', views.successful_connect, name='successful_connect'),
+
+    path('panel/auth', views.auth, name='auth'),
+    path('panel', views.index, name='index'),
+    path('panel/info', views.info, name='info'),
+    path('panel/point', views.point, name='point'),
+    path('panel/users', views.users, name='users'),
 ]
--- panel/views.py
+++ panel/views.py
@@ -10,3 +10,28 @@ def connect(request):
 @require_http_methods(["GET"])
 def successful_connect(request):
     return render(request, 'captive/successful.html')
+
+
+@require_http_methods(["GET"])
+def auth(request):
+    return render(request, 'panel/auth.html')
+
+
+@require_http_methods(["GET"])
+def index(request):
+    return render(request, 'panel/index.html')
+
+
+@require_http_methods(["GET"])
+def info(request):
+    return render(request, 'panel/info.html')
+
+
+@require_http_methods(["GET"])
+def point(request):
+    return render(request, 'panel/point.html')
+
+
+@require_http_methods(["GET"])
+def users(request):
+    return render(request, 'panel/users.html')
