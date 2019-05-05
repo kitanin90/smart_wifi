3c3a0997b2620fa18952ef5e99c240bef7e1b5ae
Viktor Plis <rigfox@gmail.com>
2019-05-05 14:36:30 +0500

    Added captive web page

--- panel/urls.py
+++ panel/urls.py
@@ -1,2 +1,8 @@
 from django.urls import path
+
 from . import views
+
+urlpatterns = [
+    path('', views.connect, name='connect'),
+    path('successful', views.successful_connect, name='successful_connect'),
+]
--- panel/views.py
+++ panel/views.py
@@ -1,1 +1,12 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+
+
+@require_http_methods(["GET"])
+def connect(request):
+    return render(request, 'captive/index.html')
+
+
+@require_http_methods(["GET"])
+def successful_connect(request):
+    return render(request, 'captive/successful.html')
