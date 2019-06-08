d85b72dc8f0524305323509249f642382661efa5
RigFox <rigfox@gmail.com>
2019-06-08 17:41:36 +0500

    Added freeradius debug

--- freeradius/entrypoint.sh
+++ freeradius/entrypoint.sh
@@ -3,4 +3,4 @@
 sed -i "s/{MYSQL_DATABASE}/$MYSQL_DATABASE/g" /etc/raddb/mods-enabled/sql
 sed -i "s/{MYSQL_ROOT_PASSWORD}/$MYSQL_ROOT_PASSWORD/g" /etc/raddb/mods-enabled/sql
 
-./docker-entrypoint.sh
+./docker-entrypoint.sh radiusd -X
