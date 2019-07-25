9de55a46f940b88ad1965bf1bfa088708d20ce4c
kitanin90 <kitanin90@mail.ru>
2019-07-25 12:05:54 +0500

    Add forms.py

---
+++ panel/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Feedback
+from django.core.exceptions import ValidationError
+
+# class FeedbackForm(forms.ModelForm):
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ['username', 'telephone', 'title', 'description']
+
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', "placeholder": "ФИО клиента"}),
+            'telephone': forms.TextInput(attrs={'class': 'form-control', "placeholder": "Номер телефона"}),
+            'title': forms.TextInput(attrs={'class': 'form-control', "placeholder": "Заголовок"}),
+            'description': forms.Textarea(attrs={'class': 'form-control', "placeholder": "Описание"}),
+        }
+
+
+class UploadFileForm(forms.ModelForm):
+    file = forms.FileField()
