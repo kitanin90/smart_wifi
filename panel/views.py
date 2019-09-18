32d6829de2ee499fea98b6247068753ce3966e6e
kitanin90 <kitanin90@mail.ru>
2019-09-18 13:23:32 +0500

    correnct log in

--- panel/views.py
+++ panel/views.py
@@ -43,8 +43,7 @@ def auth(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
-        username_login = '{}_{}'.format(username, password)
-        user = authenticate(request, username=username_login, password=password)
+        user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
